b9109c4ba0298eb7b7e6fe9354628989c26bbbb6
Hoandtn02 <121144838+Hoandtn02@users.noreply.github.com>
2024-08-12 10:59:28 +0700

    Update

---
+++ Demo_2048Game/Demo_2048Game/Demo_2048Game/Game.h
@@ -0,0 +1,22 @@
+﻿#ifndef GAME_H
+#define GAME_H
+
+#include "Board.h"
+
+class Game {
+private:
+    Board* board;
+    int currentSize; // Lưu kích thước hiện tại
+    bool useLetters; // Lưu chế độ chơi (số hoặc chữ)
+    void selectBoardSize();
+    void selectGameMode(); // Thêm khai báo hàm selectGameMode
+    void restartGame(); // Thêm khai báo hàm restartGame
+    bool isGameOver() const;
+
+public:
+    Game();
+    ~Game();
+    void play();
+};
+
+#endif // GAME_H
